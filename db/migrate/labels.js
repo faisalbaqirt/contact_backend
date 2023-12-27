debf8cbdd0e88f534b56415e280cbb788831f214
faisalbaqirt <faishalb0@gmail.com>
2023-12-27 20:46:37 +0700

    add migrate table for labels

---
+++ db/migrate/labels.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+const pool = require('../config');
+
+const createLabelTable = `
+  CREATE TABLE IF NOT EXISTS labels (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+  );
+`;
+
+pool.query(createLabelTable)
+  .then(() => console.log('Label table created successfully'))
+  .catch(err => console.error('Error creating label table:', err));
