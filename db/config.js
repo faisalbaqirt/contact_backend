ec54968b50c2772addb6c1c4eba634c77938c5c1
faisalbaqirt <faishalb0@gmail.com>
2023-12-27 20:40:20 +0700

    add config db

---
+++ db/config.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg")
+require("dotenv").config()
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: 5432
+})
+
+module.exports = pool
