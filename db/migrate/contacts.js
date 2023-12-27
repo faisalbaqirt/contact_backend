3fa08c0e2721686e4dc7d969a0cf22fd432f6846
faisalbaqirt <faishalb0@gmail.com>
2023-12-27 20:45:11 +0700

    add migrate table for contacts

---
+++ db/migrate/contacts.js
@@ -0,0 +1,19 @@
+const { Pool } = require("pg");
+const pool = require('../config');
+
+const createContactTable = `
+  CREATE TABLE IF NOT EXISTS contacts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    name VARCHAR(255) NOT NULL,
+    telephone VARCHAR(15),
+    email VARCHAR(255),
+    address VARCHAR(255),
+    labels VARCHAR(255)[] DEFAULT '{}'
+  );
+`;
+
+pool.query(createContactTable)
+  .then(() => console.log('Contact table created successfully'))
+  .catch(err => console.error('Error creating contact table:', err));
+
