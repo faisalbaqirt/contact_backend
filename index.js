cd15bdfc2acae90acec42458c674b43a326ced96
faisalbaqirt <faishalb0@gmail.com>
2023-12-27 21:51:32 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express")
 const app = express()
+const passport = require("./lib/passport")
+
+app.use(express.json())
+app.use(passport.initialize())
+
+const AuthRoute = require("./routes/authRoute")
+app.use("/api/v1/auth", AuthRoute)
 
 const port = process.env.PORT || 5000
 app.listen(port, ()=> {
