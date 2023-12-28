641454bb3bc0d15e18c65da6579f7159d7fbdcd9
faisalbaqirt <faishalb0@gmail.com>
2023-12-28 21:34:56 +0700

    Refactor function to use syntax for PostgreSQL

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,49 +1,57 @@
-const pool = require("../db/config")
-
+const pool = require("../db/config");
 
 const getAllContactList = async (userId) => {
-    try {
-        const query = "SELECT * FROM contacts WHERE user_id = ?"
-        const data = await pool.query(query, [userId])
-        return data
-    } catch (error) {
-        throw error
-    }
-}
+  try {
+    const query = "SELECT * FROM contacts WHERE user_id = $1";
+    const data = await pool.query(query, [userId]);
+    return data.rows;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const createContact = async (newContact) => {
-    try {
-        const query = "INSERT INTO contacts SET ?"
-        await pool.query(query, newContact)
-    } catch (error) {
-        throw error;
-    }
-}
+const createContact = async (
+  userId,
+  name,
+  telephone,
+  email,
+  address,
+  labels
+) => {
+  try {
+    const query =
+      "INSERT INTO contacts (user_id, name, telephone, email, address, labels) VALUES ($1, $2, $3, $4, $5, $6)";
+    const values = [userId, name, telephone, email, address, labels];
+    await pool.query(query, values);
+  } catch (error) {
+    throw error;
+  }
+};
 
-const updateContact = async (id, updatedContact) => {
-    try {
-        const query = "UPDATE contacts SET ? WHERE id = ?"
-        await pool.query(query, [updatedContact, id])
-    } catch (error) {
-        throw error;
-    }
-}
+const updateContact = async (id, name, telephone, email, address, labels) => {
+  try {
+    const query =
+      "UPDATE contacts SET name = $1, telephone = $2, email = $3, address = $4, labels = $5 WHERE id = $6";
+    const values = [name, telephone, email, address, labels, id];
+    await pool.query(query, values);
+  } catch (error) {
+    throw error;
+  }
+};
 
 const deleteContact = async (id) => {
-    try {
-        const query = "DELETE FROM contacts WHERE id = ? RETURNING id"
-        const result = await pool.query(query, [id])
-        return result[0]
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const query = "DELETE FROM contacts WHERE id = $1 RETURNING id";
+    const result = await pool.query(query, [id]);
+    return result[0];
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
-    getAllContactList,
-    createContact,
-    updateContact,
-    deleteContact
-}
-
-
+  getAllContactList,
+  createContact,
+  updateContact,
+  deleteContact,
+};
