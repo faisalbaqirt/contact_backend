84639954d04e371ca5cd2c6659e4f48c3d86cb7d
faisalbaqirt <faishalb0@gmail.com>
2023-12-28 20:04:01 +0700

    use endpoint for contact route

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(passport.initialize())
 const AuthRoute = require("./routes/authRoute")
 app.use("/api/v1/auth", AuthRoute)
 
+const ContactRoute = require("./routes/contactRoute")
+app.use("/api/v1/contact", ContactRoute)
+
 const port = process.env.PORT || 5000
 app.listen(port, ()=> {
     console.log(`listening on port ${port}`)
