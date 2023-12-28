17fe04a6879ed2bf15ed18fac4fe0f436f5a1394
faisalbaqirt <faishalb0@gmail.com>
2023-12-28 21:01:26 +0700

    add try-catch to handle errors in authModel

--- models/authModel.js
+++ models/authModel.js
@@ -1,17 +1,27 @@
 const pool = require("../db/config");
 
 const registerUser = async (username, hashedPassword) => {
-  const query =
-    "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username";
-  return await pool.query(query, [username, hashedPassword]);
+  try {
+    const query =
+      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username";
+    const result = await pool.query(query, [username, hashedPassword]);
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
 };
 
 const getByUsername = async (username) => {
-  const query = "SELECT * FROM users WHERE username = $1 LIMIT 1";
-  return await pool.query(query, [username]);
+  try {
+    const query = "SELECT * FROM users WHERE username = $1 LIMIT 1";
+    const result = await pool.query(query, [username]);
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = {
   registerUser,
-  getByUsername
+  getByUsername,
 };
