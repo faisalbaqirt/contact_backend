b78dd82cb93fff4b49428ecd0faa976ddb57398e
faisalbaqirt <faishalb0@gmail.com>
2023-12-30 21:24:41 +0700

    edit route for contact

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -6,8 +6,9 @@ const isAuthenticated = require("../middleware/isAuthenticated")
 router.use(isAuthenticated)
 
 router.get("/", ContactController.getAllContactList)
-router.post("/", ContactController.createContact)
-router.put("/:id", ContactController.updateContact)
-router.delete("/:id", ContactController.deleteContact)
+router.get("/person/:id", ContactController.getContactById)
+router.post("/new", ContactController.createContact)
+router.put("/person/:id", ContactController.updateContact)
+router.delete("/person/:id", ContactController.deleteContact)
 
 module.exports = router
