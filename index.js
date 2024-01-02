15d02595b8acc8dbb46900b2e5008eee91f67c80
faisalbaqirt <faishalb0@gmail.com>
2024-01-02 19:35:38 +0700

    use labelroute

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use("/api/v1/auth", AuthRoute)
 const ContactRoute = require("./routes/contactRoute")
 app.use("/api/v1/contact", ContactRoute)
 
+const LabelRoute = require("./routes/labelRoute")
+app.use("/api/v1/label", LabelRoute)
+
 const port = process.env.PORT || 5000
 app.listen(port, ()=> {
     console.log(`listening on port ${port}`)
